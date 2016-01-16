600bbae8d36093b8cfa6c1d589473d22eb261054
fnwiya <fnwiya@gmail.com>
2016-01-16 13:58:13 +0900

    update

---
+++ python/_nlp25.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# import sys
+# import re
+
+
+# def nlp25(filename):
+#     base_info_pattarn = re.compile(r"")
+#     ret = {}
+#     with open(filename) as f:
+#         lines = f.readlines()
+#         for line in lines:
+#             base_info = base_info_pattarn.search(line)
+#             if base_info:
+#                 ret[base_info.group()] = base_info.group()
+#     return ret
